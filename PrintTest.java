ec891ae314345b50186cfc141f4f91e4a00e147a
Hunter <hantek.f@gmail.com>
2017-01-11 20:20:14 +0100

    first commit

---
+++ PrintTest.java
@@ -0,0 +1,8 @@
+package printtest;
+
+public class PrintTest {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+    }
+}
---
+++ Test.java
@@ -0,0 +1,5 @@
+package printtest;
+
+public class Test {
+    {System.out.println("hello");}
+}
